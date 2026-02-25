9a56dfc7e40a55b881537ebacc501ad931752faa
Silviu <183800984+silviu28@users.noreply.github.com>
2026-02-25 15:04:08 +0200

    add coming soon

---
+++ src/components/ComingSoon.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+const Centered = styled.div`
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`
+
+const ComingSoon = () => {
+  return (
+    <Centered>
+      <div class="tenor-gif-embed" data-postid="9181230269029198964" data-share-method="host" data-aspect-ratio="1.00403" data-width="100%"><a href="https://tenor.com/view/hmm-gif-9181230269029198964">Hmm Sticker</a>from <a href="https://tenor.com/search/hmm-stickers">Hmm Stickers</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+      <h2>Coming soon...</h2>
+      <p>This page is in construction</p>
+    </Centered>
+  )
+}
+
+export default ComingSoon
